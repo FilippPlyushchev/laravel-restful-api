c32800eea73f59d7b0a722b6f7fda1384a0d88e2
FilippPlyushchev <noolkmvm@gmail.com>
2020-10-19 07:28:56 +0700

    receiving by id

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -11,4 +11,8 @@ class CountryController extends Controller
     public function country() {
         return response()->json(Country::get(), 200);
     }
+
+    public function countryById($id) {
+        return response()->json(Country::find($id), 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('country', [CountryController::class, 'country']);
+Route::get('country/{id}', [CountryController::class, 'countryById']);
+
